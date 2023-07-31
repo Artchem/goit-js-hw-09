4b5ecf398c36bd3cb65ac593231ff9245f0b5e17
Artchem <127445887+Artchem@users.noreply.github.com>
2023-07-31 18:48:17 +0000

    Deploying to gh-pages from @ Artchem/goit-js-hw-09@d51aea0cf00aebba111e9aac66bbb237c19269cf 🚀

---
+++ 01-color-switcher.42574aa3.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")},n=null;t.btnStart.addEventListener("click",(function(a){n=setInterval((function(){a.target.disabled=!0,t.btnStop.disabled=!1,document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.btnStop.addEventListener("click",(function(a){a.target.disabled=!0,t.btnStart.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.42574aa3.js.map
